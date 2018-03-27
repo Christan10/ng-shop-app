6d820ad03e9156d83125aad689393598f4555926
ch.skordas <ch.skordas@stoiximan.gr>
2018-03-27 18:06:24 +0300

    refactor(module): change display on app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -14,40 +16,49 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RecipeService } from './recipes/recipe.service';
-import { HttpClientModule } from '@angular/common/http';
 import { DataStorageService } from './shared/data-storage.service';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import { AuthService } from './auth/auth.service';
 import { AuthGuard } from './auth/auth-guard.service';
 
+const MODULES = [
+  BrowserModule,
+  AppRoutingModule,
+  HttpClientModule,
+  FormsModule,
+  ReactiveFormsModule
+];
+
+const SERVICES = [
+  ShoppingListService,
+  RecipeService,
+  DataStorageService,
+  AuthService,
+  AuthGuard
+];
+
+const COMPONENTS = [
+  AppComponent,
+  HeaderComponent,
+  RecipesComponent,
+  RecipeListComponent,
+  RecipeDetailComponent,
+  RecipeItemComponent,
+  ShoppingListComponent,
+  ShoppingEditComponent,
+  DropdownDirective,
+  RecipeStartComponent,
+  RecipeEditComponent,
+  SignupComponent,
+  SigninComponent
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
-    DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
-    SignupComponent,
-    SigninComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule
-  ],
-  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
+  declarations: COMPONENTS,
+  imports: MODULES,
+  providers: SERVICES,
   bootstrap: [AppComponent]
 })
 export class AppModule { }

645def37c519bf4b8ba45b20fe859e716ededbac
ch.skordas <ch.skordas@stoiximan.gr>
2018-04-04 14:55:59 +0300

    fear(modules): create auth module and routing modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { RecipesModule } from './recipes/recipes.module';
 
+import { RecipesModule } from './recipes/recipes.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeService } from './recipes/recipe.service';
 import { DataStorageService } from './shared/data-storage.service';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
 import { AuthService } from './auth/auth.service';
 import { AuthGuard } from './auth/auth-guard.service';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { AuthModule } from './auth/auth.module';
 
 
 const MODULES = [
@@ -23,7 +22,8 @@ const MODULES = [
   HttpClientModule,
   RecipesModule,
   SharedModule,
-  ShoppingListModule
+  ShoppingListModule,
+  AuthModule
 ];
 
 const SERVICES = [
@@ -37,8 +37,6 @@ const SERVICES = [
 const COMPONENTS = [
   AppComponent,
   HeaderComponent,
-  SignupComponent,
-  SigninComponent
 ];
 
 @NgModule({
